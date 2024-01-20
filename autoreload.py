68360b5f5ecb211c28c17a3fbe0b23469ac3c29c
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-20 18:17:48 +0100

    basic operators

--- autoreload.py
+++ autoreload.py
@@ -10,7 +10,7 @@ class MyHandler(FileSystemEventHandler):
             restart_script()
 
 def restart_script():
-    subprocess.run(["python", "list.py"])
+    subprocess.run(["python", "basic_operators.py"])
 
 def watch():
     event_handler = MyHandler()
---
+++ basic_operators.py
@@ -0,0 +1,5 @@
+numbers = 1 + 2 * 3 / 4.0
+print(numbers)
+
+remainder = 11 % 3
+print(remainder)
