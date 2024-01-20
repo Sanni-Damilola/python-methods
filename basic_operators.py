26ce4ee1e5a5898c06048acd865734aeb2b722b8
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-20 18:27:10 +0100

    basic operators

--- basic_operators.py
+++ basic_operators.py
@@ -11,9 +11,19 @@ cubed = 2 ** 3
 print(squred)
 print(cubed)
 
-# Using Operators with Strings
 
+# Using Operators with Strings
 helloworld = ("hello world")
 print(helloworld)
 
+lotOfHellos = "Hello " * 10
+print(lotOfHellos)
 
+
+# Using Operators with Strings
+even_numbers = [2,4,6,8]
+odd_numers = [1,3,5,7]
+all_numbers = odd_numers + even_numbers
+print(all_numbers)
+
+print([1,2,3] * 3)
