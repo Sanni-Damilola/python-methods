efe74fa04d4d7a54acd0050fef0f65d13f5119d4
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-20 18:21:21 +0100

    basic operators

--- basic_operators.py
+++ basic_operators.py
@@ -1,5 +1,19 @@
+
+# Arithmetic Operators
 numbers = 1 + 2 * 3 / 4.0
 print(numbers)
 
 remainder = 11 % 3
 print(remainder)
+
+squred = 7 ** 2
+cubed = 2 ** 3
+print(squred)
+print(cubed)
+
+# Using Operators with Strings
+
+helloworld = ("hello world")
+print(helloworld)
+
+
