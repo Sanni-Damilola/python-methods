429387bc3842eb7b4ca93018130f902b529a51ee
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-20 18:04:59 +0100

    variables & types done

--- variables.py
+++ variables.py
@@ -30,3 +30,13 @@ world = "world"
 
 helloWorld = hello + " " + world
 print(helloWorld)
+
+a, b = 3, 4
+print(a ,b)
+
+# This will not work!
+one = 1
+two = 2
+hello = "hello"
+
+# print(one + two + hello)
