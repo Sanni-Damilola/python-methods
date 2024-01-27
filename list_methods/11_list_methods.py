b8e9277a68b6030de6fe47d109e15048cbaca51f
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-27 10:47:58 +0100

    11 list methods

--- list_methods/11_list_methods.py
+++ list_methods/11_list_methods.py
@@ -1,6 +1,25 @@
 # 1: Append
-people = ["Carl Johnson", "Sweet", "Big Smoke"]
+people = ["Carl Johnson", "Sweet", "Sweet", "Big Smoke"]
 people.append("Sanni")
-print(people)
+# print(people)
 
 # 2: Clear
+# people.clear()
+# print(people)
+
+# 3: copy
+copy_people: list[str] = people.copy()
+print(copy_people)
+copy_people.remove("Sweet")
+print(copy_people)
+
+# 4: count
+print(people.count("Sweet"))
+
+# 5: extend
+extend_list : list[str] = ["A", "B", "C"]
+extend_list1: list[int] = [1, 2, 3, 4,]
+extend_list.extend(extend_list1)
+print(extend_list)
+
+# 6:
