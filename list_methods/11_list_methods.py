e72eeba55a28f9065d74ce8cb8f2ffdcd4a03b0f
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-27 10:33:33 +0100

    11 list methods

--- list_methods/11_list_methods.py
+++ list_methods/11_list_methods.py
@@ -1,9 +1,9 @@
 def get_all_list_methods():
     count = 0
     for method in dir(list):
-        count += 1
-        print(count, method)
-
+        if not "__" in method:
+            count += 1
+            print(count, method)
 
 get_all_list_methods()
 
